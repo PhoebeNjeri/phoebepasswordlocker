049ba7b4013ada6763b305f7916e6f83d4acc147
PhoebeNjeri <Phoebegichuhi4@gmail.com>
2020-09-07 11:03:13 +0300

    all changes

--- account.py
+++ account.py
@@ -1,3 +1,23 @@
 import string
 import pyperclip
 import random
+class User:
+    """
+    Class that generates new instances of user's information
+    """
+    
+    user_list =[]
+
+    def __init__(self,lockname,password):
+
+        self.lockname = lockname
+        self.password = password
+
+    def save_user(self):
+
+        '''
+        save_user method saves user's information objects into user_list
+        '''
+
+        User.user_list.append(self)
+        
